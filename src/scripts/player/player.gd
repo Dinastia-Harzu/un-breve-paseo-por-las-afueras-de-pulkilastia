9fb840880d30dabe5833e3d25a132b805b834e79
Dinastía Harzu αγρ <dinastiadelah@gmail.com>
2026-09-04 03:13:00 +0200

    Modificar jugador

--- src/scripts/player/player.gd
+++ src/scripts/player/player.gd
@@ -5,6 +5,11 @@ extends CharacterBody2D
 @export var speed: float = 100.
 
 @export var sprite: Sprite2D
+@export var remote_transform: RemoteTransform2D
+
+
+func _ready() -> void:
+	add_to_group(NodeGroups.WORLD_ENTITIES)
 
 
 func _physics_process(delta: float) -> void:
@@ -19,6 +24,8 @@ func _physics_process(delta: float) -> void:
 	else:
 		velocity = velocity.move_toward(Vector2.ZERO, speed)
 
-	Log.info("%.2v" % global_position)
-
 	move_and_slide()
+
+
+func assign_camera(camera: Camera2D) -> void:
+	remote_transform.remote_path = camera.get_path()
