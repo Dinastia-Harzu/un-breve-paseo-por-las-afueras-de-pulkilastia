c4108583c2d6ae31a830c3bdd660233863f85d7a
Dinastía Harzu αγρ <dinastiadelah@gmail.com>
2026-09-08 20:13:18 +0200

    Añadir recursos para datos de enemigos

--- src/scripts/enemies/base/map_enemy.gd
+++ src/scripts/enemies/base/map_enemy.gd
@@ -2,5 +2,8 @@ class_name MapEnemy
 extends CharacterBody2D
 
 
+@export var enemy_data: EnemyData
+
+
 func _ready() -> void:
 	add_to_group(NodeGroups.WORLD_ENTITIES)
---
+++ src/scripts/resources/enemy_data.gd
@@ -0,0 +1,14 @@
+class_name EnemyData
+extends Resource
+
+
+@export_group("Identity")
+@export var name: String
+
+@export_group("Base Stats")
+@export var max_hp: int
+@export var offense: int
+@export var defense: int
+@export var intelligence: int
+@export var constitution: int
+@export var speed: int
